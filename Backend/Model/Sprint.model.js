742c04e2590cde9ef677f290037fb97f34239a17
obaidrehman30 <obaidrehman3009@gmail.com>
2025-09-05 16:58:03 -0400

    Update Sprint.model

--- Backend/Model/Sprint.model.js
+++ Backend/Model/Sprint.model.js
@@ -1,19 +1,1 @@
-const mongoose = require("mongoose")
-
-const sprintSchema = mongoose.Schema({
-    title:String,
-    description:String,
-    startDate:String,
-    targetDate:String,
-    favourite:Boolean,
-    users:[],
-    status:Boolean,
-    userID:String,
-})
-
-const SprintModel = mongoose.model("sprint", sprintSchema)
-
-module.exports = {
-    SprintModel
-}
 
