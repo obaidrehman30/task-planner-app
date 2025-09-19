f065b79030dbcf66ff081af3830f8428ce0c862c
obaidrehman30 <obaidrehman3009@gmail.com>
2025-09-19 16:23:14 -0400

    Update db

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,8 +1,1 @@
 
-const mongoose=require("mongoose");
-mongoose.set("strictQuery", false);
-require('dotenv').config();
-
-const connection = mongoose.connect(process.env.mongoURL);
-
-module.exports = {connection}
